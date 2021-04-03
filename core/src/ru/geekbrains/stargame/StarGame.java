be5fb4149f3488494f611e0530611af638cb7ac4
EvdokimovViktor <evdokimov.viktor.91@mail.ru>
2021-04-03 11:30:31 +0500

    Copy project

--- core/src/ru/geekbrains/stargame/StarGame.java
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -1,95 +1,14 @@
 package ru.geekbrains.stargame;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 
-public class StarGame extends ApplicationAdapter implements InputProcessor {
-	SpriteBatch batch;
-	Texture img;
-	int coordinateX;
-	int coordinateY;
-	Vector2 direction = new Vector2(0, 0);
-	Vector2 position = new Vector2(0, 0);
+import com.badlogic.gdx.Game;
 
+import ru.geekbrains.stargame.screen.MenuScreen;
+
+public class StarGame extends Game {
 
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("backGround.jpg");
-		Gdx.input.setInputProcessor(this);
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		getDirection();
-		if (Math.abs(position.x-coordinateX)<direction.x && Math.abs(position.y-coordinateY)<direction.y) {
-			direction.set(0,0);
-		}
-		position.add(direction);
-		batch.draw(img, position.x, position.y);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		coordinateX=screenX;
-		coordinateY=Gdx.graphics.getHeight()-screenY;
-		getDirection();
-		return false;
-	}
-
-	private void getDirection (){
-		direction.set(coordinateX, coordinateY);
-		direction.sub(position);
-		direction.nor();
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(float amountX, float amountY) {
-		return false;
+	public void create() {
+		setScreen(new MenuScreen());
 	}
 }
---
+++ core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.stargame.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.stargame.base.BaseScreen;
+import ru.geekbrains.stargame.math.Rect;
+import ru.geekbrains.stargame.sprite.Background;
+
+public class MenuScreen extends BaseScreen {
+
+    private Texture bg;
+    private Background background;
+
+    @Override
+    public void show() {
+        super.show();
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        background.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        bg.dispose();
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        return false;
+    }
+}
